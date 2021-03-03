a55b77abff5f9720720b69d552f90283c2f638fd
Dane Murphy <danem7@gmail.com>
2021-03-03 15:03:55 -0800

    react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          A change
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path={'/home'} component={Home}/>
+      <Route exact path={'/about'} component={About}/>
+    </Router>
+  )
+}
+
+function Home() {
+  return (
+    <h1>This is the home page</h1>
+  )
+}
+
+function About() {
+  return (
+    <h1>This is the about page</h1>
+  )
 }
 
 export default App;
