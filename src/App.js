4a763f1e4233c459ae2fdc281a8af54c623c2f5e
Dane Murphy <danem7@gmail.com>
2021-03-03 15:10:57 -0800

    changed a route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
 
 
 function App() {
   return (
     <Router>
-      <Route exact path={'/home'} component={Home}/>
-      <Route exact path={'/about'} component={About}/>
+      <Switch>
+        <Route exact path={'/'} component={Home}/>
+        <Route exact path={'/about'} component={About}/>
+      </Switch>
     </Router>
   )
 }
